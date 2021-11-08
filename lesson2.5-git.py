48081e9c939155b4e239bb2676985c36422cfa5b
Rust <rust69@list.ru>
2021-11-08 21:27:46 +0300

    initial

---
+++ lesson2.5-git.py
@@ -0,0 +1,68 @@
+print('Задача 9. Недоделка')
+from datetime import datetime
+
+def random(N):
+  current_time = datetime.now().time()
+  enpy = current_time.microsecond
+  return enpy % N + 1
+
+def rock_paper_scissors():
+  machineStep = random(3) # камень - 1, ножницы - 2, бумага - 3
+
+  print('Введите ваш ход: камень, ножницы или бумага \nДля выхода в меню введите 0')
+  string = input()
+  if string == '0':
+    mainMenu()
+  elif (string == 'Камень' or string == 'камень'):
+    if machineStep == 1:
+      print('Камень |', string)
+    elif machineStep == 2:
+      print('Ножницы |', string)
+      print('!!!Вы выиграли!!!')
+    else:
+      print('Бумага |', string)
+      print('Компьютер выиграл')
+  elif (string == 'Ножницы' or string == 'ножницы'):
+    if machineStep == 1:
+      print('Камень |', string)
+      print('Компьютер выиграл')
+    elif machineStep == 2:
+      print('Ножницы |', string)
+    else:
+      print('Бумага |', string)
+      print('!!!Вы выиграли!!!')
+  elif (string == 'Бумага' or string == 'бумага'):
+    if machineStep == 1:
+      print('Камень |', string)
+      print('!!!Вы выиграли!!!')
+    elif machineStep == 2:
+      print('Ножницы |', string)
+      print('Компьютер выиграл')
+    else:
+      print('Бумага |', string)
+  else:
+    print('Ошибка ввода!')
+  rock_paper_scissors()
+
+def guess_the_number():
+  hidden_number = random(10) # машина выбирает число от 1 до 10
+
+  number = int(input('Введите число от 1 до 10: '))
+  if number == hidden_number:
+    print('Угадал)))')
+    mainMenu()
+  else:
+    guess_the_number()
+
+def mainMenu():
+    print('Выберите игру: \n 1 - "Камень, ножницы, бумага" \n 2- "Угадай число"')
+    game = int(input())
+    if game == 1:
+      rock_paper_scissors()
+    elif game == 2:
+      guess_the_number()
+    else:
+      print('Ошибка ввода!')
+      mainMenu()
+
+mainMenu()
